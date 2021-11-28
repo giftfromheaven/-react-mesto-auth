11739dcb28b9728d5a21cf5934fb23a2635825fc
Ilya Dykin <dddiii06@yandex.ru>
2021-11-28 23:28:16 +0300

    first fix

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -26,7 +26,7 @@ export const EditProfilePopup = ({ isOpen, onClose, onUpdateUser, onCloseOverlay
   useEffect(() => {
     setName(currentUser.name);
     setDescription(currentUser.about);
-  }, [currentUser]);
+  }, [currentUser, isOpen]);
 
   return (
     <PopupWithForm
